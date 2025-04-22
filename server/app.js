ca21ee07756722d0c82ea54b8f9857a3e1381c72
ashutosh sao <63497482+ashutoshsao@users.noreply.github.com>
2025-04-22 23:38:25 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ const server = createServer(app);
 
 const io = new Server(server,{
     cors:{
-        origin:"http://localhost:5173",
+        origin:"https://chat-app-client-f43b.onrender.com",
         methods:["GET","POST"],
         credentials:true,
     },
